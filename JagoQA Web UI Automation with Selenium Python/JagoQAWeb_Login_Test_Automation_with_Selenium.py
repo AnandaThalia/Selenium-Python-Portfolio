bfad6fa3dc59d3f4c72c56d9c93688effd5cd889
ananda_thalia <ananda.thalia1701@gmail.com>
2025-01-21 07:56:31 +0700

    adjust folder

--- JagoQA Web UI Automation with Selenium Python/JagoQAWeb_Login_Test_Automation_with_Selenium.py
+++ JagoQA Web UI Automation with Selenium Python/JagoQAWeb_Login_Test_Automation_with_Selenium.py
@@ -32,146 +32,146 @@ class TestLogin(unittest.TestCase):
         self.assertEqual(respon_welcome, "Welcome Jago QA")
         self.assertEqual(respon_berhasil, "Anda Berhasil Login")
 
-    def test_b_failed_login_email_not_registered(self):
-        browser = self.browser
-        browser.get("http://barru.pythonanywhere.com/daftar")
-        time.sleep(1)
-        browser.find_element(
-            By.ID, "email").send_keys("nanda@jagoqa.com")
-        time.sleep(1)
-        browser.find_element(By.ID, "password").send_keys("sman60jakarta")
-        time.sleep(1)
-        browser.find_element(
-            By.ID, "signin_login").click()
-        time.sleep(2)
-        respon_welcome = browser.find_element(
-            By.ID, "swal2-title").text
-        respon_berhasil = browser.find_element(
-            By.ID, "swal2-content").text
-        self.assertEqual(respon_welcome, "User's not found")
-        self.assertEqual(respon_berhasil, "Email atau Password Anda Salah")
+    # def test_b_failed_login_email_not_registered(self):
+    #     browser = self.browser
+    #     browser.get("http://barru.pythonanywhere.com/daftar")
+    #     time.sleep(1)
+    #     browser.find_element(
+    #         By.ID, "email").send_keys("nanda@jagoqa.com")
+    #     time.sleep(1)
+    #     browser.find_element(By.ID, "password").send_keys("sman60jakarta")
+    #     time.sleep(1)
+    #     browser.find_element(
+    #         By.ID, "signin_login").click()
+    #     time.sleep(2)
+    #     respon_welcome = browser.find_element(
+    #         By.ID, "swal2-title").text
+    #     respon_berhasil = browser.find_element(
+    #         By.ID, "swal2-content").text
+    #     self.assertEqual(respon_welcome, "User's not found")
+    #     self.assertEqual(respon_berhasil, "Email atau Password Anda Salah")
 
-    def test_c_failed_login_empty_email_and_password(self):
-        browser = self.browser
-        browser.get("http://barru.pythonanywhere.com/daftar")
-        time.sleep(1)
-        browser.find_element(
-            By.ID, "signin_login").click()
-        time.sleep(2)
-        respon_welcome = browser.find_element(
-            By.ID, "swal2-title").text
-        respon_berhasil = browser.find_element(
-            By.ID, "swal2-content").text
-        self.assertEqual(respon_welcome, "Cek Formulir Anda")
-        self.assertEqual(respon_berhasil, "Email & Password tidak boleh kosong")
+    # def test_c_failed_login_empty_email_and_password(self):
+    #     browser = self.browser
+    #     browser.get("http://barru.pythonanywhere.com/daftar")
+    #     time.sleep(1)
+    #     browser.find_element(
+    #         By.ID, "signin_login").click()
+    #     time.sleep(2)
+    #     respon_welcome = browser.find_element(
+    #         By.ID, "swal2-title").text
+    #     respon_berhasil = browser.find_element(
+    #         By.ID, "swal2-content").text
+    #     self.assertEqual(respon_welcome, "Cek Formulir Anda")
+    #     self.assertEqual(respon_berhasil, "Email & Password tidak boleh kosong")
 
 
-    def test_d_failed_login_empty_email(self):
-        browser = self.browser
-        browser.get("http://barru.pythonanywhere.com/daftar")
-        time.sleep(1)
-        browser.find_element(By.ID, "password").send_keys("sman60jakarta")
-        time.sleep(1)
-        browser.find_element(
-            By.ID, "signin_login").click()
-        time.sleep(2)
-        respon_welcome = browser.find_element(
-            By.ID, "swal2-title").text
-        respon_berhasil = browser.find_element(
-            By.ID, "swal2-content").text
-        self.assertEqual(respon_welcome, "Cek Formulir Anda")
-        self.assertEqual(respon_berhasil, "Email tidak boleh kosong")
+    # def test_d_failed_login_empty_email(self):
+    #     browser = self.browser
+    #     browser.get("http://barru.pythonanywhere.com/daftar")
+    #     time.sleep(1)
+    #     browser.find_element(By.ID, "password").send_keys("sman60jakarta")
+    #     time.sleep(1)
+    #     browser.find_element(
+    #         By.ID, "signin_login").click()
+    #     time.sleep(2)
+    #     respon_welcome = browser.find_element(
+    #         By.ID, "swal2-title").text
+    #     respon_berhasil = browser.find_element(
+    #         By.ID, "swal2-content").text
+    #     self.assertEqual(respon_welcome, "Cek Formulir Anda")
+    #     self.assertEqual(respon_berhasil, "Email tidak boleh kosong")
 
-    def test_e_failed_login_empty_password(self):
-        browser = self.browser
-        browser.get("http://barru.pythonanywhere.com/daftar")
-        time.sleep(1)
-        browser.find_element(
-            By.ID, "email").send_keys("nanda@jagoqa.com")
-        time.sleep(1)
-        browser.find_element(
-            By.ID, "signin_login").click()
-        time.sleep(2)
-        respon_welcome = browser.find_element(
-            By.ID, "swal2-title").text
-        respon_berhasil = browser.find_element(
-            By.ID, "swal2-content").text
-        self.assertEqual(respon_welcome, "Cek Formulir Anda")
-        self.assertEqual(respon_berhasil, "Password tidak boleh kosong")
+    # def test_e_failed_login_empty_password(self):
+    #     browser = self.browser
+    #     browser.get("http://barru.pythonanywhere.com/daftar")
+    #     time.sleep(1)
+    #     browser.find_element(
+    #         By.ID, "email").send_keys("nanda@jagoqa.com")
+    #     time.sleep(1)
+    #     browser.find_element(
+    #         By.ID, "signin_login").click()
+    #     time.sleep(2)
+    #     respon_welcome = browser.find_element(
+    #         By.ID, "swal2-title").text
+    #     respon_berhasil = browser.find_element(
+    #         By.ID, "swal2-content").text
+    #     self.assertEqual(respon_welcome, "Cek Formulir Anda")
+    #     self.assertEqual(respon_berhasil, "Password tidak boleh kosong")
 
-    def test_f_failed_login_email_too_long(self):
-        browser = self.browser
-        browser.get("http://barru.pythonanywhere.com/daftar")
-        time.sleep(1)
-        browser.find_element(
-            By.ID, "email").send_keys("wxsmgswoylgxmgeqzkongnlpghkxlulahcvdamudbypfwlbkizmfwvezbmebxtexnqhxanpcibddolrxgswtnhaavwseabkxjvmdmlaenjria@jagoqa.com")
-        time.sleep(1)
-        browser.find_element(By.ID, "password").send_keys("sman60jakarta")
-        time.sleep(1)
-        browser.find_element(
-            By.ID, "signin_login").click()
-        time.sleep(2)
-        respon_welcome = browser.find_element(
-            By.ID, "swal2-title").text
-        respon_berhasil = browser.find_element(
-            By.ID, "swal2-content").text
-        self.assertEqual(respon_welcome, "User's not found")
-        self.assertEqual(respon_berhasil, "Email atau Password Anda Salah")
+    # def test_f_failed_login_email_too_long(self):
+    #     browser = self.browser
+    #     browser.get("http://barru.pythonanywhere.com/daftar")
+    #     time.sleep(1)
+    #     browser.find_element(
+    #         By.ID, "email").send_keys("wxsmgswoylgxmgeqzkongnlpghkxlulahcvdamudbypfwlbkizmfwvezbmebxtexnqhxanpcibddolrxgswtnhaavwseabkxjvmdmlaenjria@jagoqa.com")
+    #     time.sleep(1)
+    #     browser.find_element(By.ID, "password").send_keys("sman60jakarta")
+    #     time.sleep(1)
+    #     browser.find_element(
+    #         By.ID, "signin_login").click()
+    #     time.sleep(2)
+    #     respon_welcome = browser.find_element(
+    #         By.ID, "swal2-title").text
+    #     respon_berhasil = browser.find_element(
+    #         By.ID, "swal2-content").text
+    #     self.assertEqual(respon_welcome, "User's not found")
+    #     self.assertEqual(respon_berhasil, "Email atau Password Anda Salah")
 
-    def test_g_failed_login_password_too_long(self):
-        browser = self.browser
-        browser.get("http://barru.pythonanywhere.com/daftar")
-        time.sleep(1)
-        browser.find_element(
-            By.ID, "email").send_keys("nanda@jagoqa.com")
-        time.sleep(1)
-        browser.find_element(By.ID, "password").send_keys(
-            "wxsmgswoylgxmgeqzkongnlpghkxlulahcvdamudbypfwlbkizmfwvezbmebxtexnqhxanpcibddolrxgswtnhaavwseabkxjvmdmlaenjria")
-        time.sleep(1)
-        browser.find_element(
-            By.ID, "signin_login").click()
-        time.sleep(2)
-        respon_welcome = browser.find_element(
-            By.ID, "swal2-title").text
-        respon_berhasil = browser.find_element(
-            By.ID, "swal2-content").text
-        self.assertEqual(respon_welcome, "User's not found")
-        self.assertEqual(respon_berhasil, "Email atau Password Anda Salah")
+    # def test_g_failed_login_password_too_long(self):
+    #     browser = self.browser
+    #     browser.get("http://barru.pythonanywhere.com/daftar")
+    #     time.sleep(1)
+    #     browser.find_element(
+    #         By.ID, "email").send_keys("nanda@jagoqa.com")
+    #     time.sleep(1)
+    #     browser.find_element(By.ID, "password").send_keys(
+    #         "wxsmgswoylgxmgeqzkongnlpghkxlulahcvdamudbypfwlbkizmfwvezbmebxtexnqhxanpcibddolrxgswtnhaavwseabkxjvmdmlaenjria")
+    #     time.sleep(1)
+    #     browser.find_element(
+    #         By.ID, "signin_login").click()
+    #     time.sleep(2)
+    #     respon_welcome = browser.find_element(
+    #         By.ID, "swal2-title").text
+    #     respon_berhasil = browser.find_element(
+    #         By.ID, "swal2-content").text
+    #     self.assertEqual(respon_welcome, "User's not found")
+    #     self.assertEqual(respon_berhasil, "Email atau Password Anda Salah")
 
-    def test_h_failed_login_invalid_email_format(self):
-        browser = self.browser
-        browser.get("http://barru.pythonanywhere.com/daftar")
-        time.sleep(1)
-        browser.find_element(
-            By.ID, "email").send_keys("nanda@@jagoqa.com")
-        time.sleep(1)
-        browser.find_element(By.ID, "password").send_keys("wxsmgswoylgxmgeqzk")
-        time.sleep(1)
-        browser.find_element(
-            By.ID, "signin_login").click()
-        time.sleep(2)
-        email_error = browser.find_element(By.ID, "email").get_attribute("validationMessage")
-        print(email_error)
-        self.assertIn("A part following '@' should not contain the symbol '@'.", email_error)
+    # def test_h_failed_login_invalid_email_format(self):
+    #     browser = self.browser
+    #     browser.get("http://barru.pythonanywhere.com/daftar")
+    #     time.sleep(1)
+    #     browser.find_element(
+    #         By.ID, "email").send_keys("nanda@@jagoqa.com")
+    #     time.sleep(1)
+    #     browser.find_element(By.ID, "password").send_keys("wxsmgswoylgxmgeqzk")
+    #     time.sleep(1)
+    #     browser.find_element(
+    #         By.ID, "signin_login").click()
+    #     time.sleep(2)
+    #     email_error = browser.find_element(By.ID, "email").get_attribute("validationMessage")
+    #     print(email_error)
+    #     self.assertIn("A part following '@' should not contain the symbol '@'.", email_error)
 
-    def test_i_failed_login_password_contain_symbol(self):
-        browser = self.browser
-        browser.get("http://barru.pythonanywhere.com/daftar")
-        time.sleep(1)
-        browser.find_element(
-            By.ID, "email").send_keys("nanda@jagoqa.com")
-        time.sleep(1)
-        browser.find_element(By.ID, "password").send_keys("wxsmgsw&@jria")
-        time.sleep(1)
-        browser.find_element(
-            By.ID, "signin_login").click()
-        time.sleep(2)
-        respon_welcome = browser.find_element(
-            By.ID, "swal2-title").text
-        respon_berhasil = browser.find_element(
-            By.ID, "swal2-content").text
-        self.assertEqual(respon_welcome, "User's not found")
-        self.assertEqual(respon_berhasil, "Email atau Password Anda Salah")
+    # def test_i_failed_login_password_contain_symbol(self):
+    #     browser = self.browser
+    #     browser.get("http://barru.pythonanywhere.com/daftar")
+    #     time.sleep(1)
+    #     browser.find_element(
+    #         By.ID, "email").send_keys("nanda@jagoqa.com")
+    #     time.sleep(1)
+    #     browser.find_element(By.ID, "password").send_keys("wxsmgsw&@jria")
+    #     time.sleep(1)
+    #     browser.find_element(
+    #         By.ID, "signin_login").click()
+    #     time.sleep(2)
+    #     respon_welcome = browser.find_element(
+    #         By.ID, "swal2-title").text
+    #     respon_berhasil = browser.find_element(
+    #         By.ID, "swal2-content").text
+    #     self.assertEqual(respon_welcome, "User's not found")
+    #     self.assertEqual(respon_berhasil, "Email atau Password Anda Salah")
 
     def tearDown(self):
         self.browser.close()
